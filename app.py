0d7196486e199ca2ef0f9c956cd64d99902d05b3
Maoz <maozkamer2@gmail.com>
2026-04-17 12:42:27 +0300

    fix audio

--- app.py
+++ app.py
@@ -590,6 +590,6 @@ def concept_map():
     return jsonify(data)
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
